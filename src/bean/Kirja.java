665c0b27cb4264135aaf165a88de621d282e23fc
MissAlways <ainakiurus@gmail.com>
2016-11-29 11:40:32 +0200

    Fix Kirja bean

--- src/bean/Kirja.java
+++ src/bean/Kirja.java
@@ -7,9 +7,13 @@ public class Kirja {
 	private int painos;
 	private String kustantaja;
 	
-	public Kirja()
-	{
-		super();
+	
+	public Kirja() {
+		isbn = "";
+		nimi = "";
+		kirjoittaja = "";
+		painos = 0;
+		kustantaja = "";
 	}
 	public Kirja (String isbn, String nimi, String kirjoittaja, int painos, String kustantaja)
 	{
@@ -23,124 +27,37 @@ public class Kirja {
 		return isbn;
 	}
 	public void setIsbn(String isbn) {
-		this.isbn = muokkaaISBN(isbn);
-	}
-	// muoto 2323-2323-232 pituus 13 merkki�
-	private String muokkaaISBN(String isbn)
-	{
-		String paluu = null;
-		
-		if (isbn != null && isbn.matches("([0-9]-?){2,13}") && isbn.length() == 13)
-			paluu = isbn;
-			
-		return paluu;
-	}
-	private  String muokkaaNimi(String nimi)
-	{
-		String paluu = null;
-		int i;
-		String sana;
-		
-		if (nimi != null && nimi.trim().length() > 0)
-		{
-			// turhat v�lit pois
-			nimi = nimi.trim();
-			nimi = nimi.replaceAll("\\s+", " ");
-			nimi = nimi.replaceAll(" - ", "-");
-			nimi = nimi.replaceAll("- ", "-");
-			nimi = nimi.replaceAll(" -", "-");
-			
-			if ( nimi.matches ("([a-zåöäA-ZÅÖÄ 0-9]-?){2,20}"))
-				
-				paluu = nimi.substring(0,1).toUpperCase()+nimi.substring(1);
-		}
-		
-		return paluu;
-	}
-	private String muokkaaHenkiloNimi(String nimi)
-	{
-		String paluu = null, apu;
-		int i;
-		String sana;
-		
-		if (nimi != null && nimi.trim().length() > 0)			
-		{
-			// turhat v�lit pois
-			nimi = nimi.trim();
-			nimi = nimi.replaceAll("\\s+", " ");
-			nimi = nimi.replaceAll(" - ", "-");
-			nimi = nimi.replaceAll("- ", "-");
-			nimi = nimi.replaceAll(" -", "-");
-			
-			//System.out.println(nimi);
-			if (nimi.matches ("([a-zåöäA-ZÅÖÄ ]-?){2,30}"))
-			{
-				nimi = nimi.toLowerCase();
-				apu = "";
-				// jokainen sana alkaa isolla
-				i = nimi.indexOf(' ');
-				while (i != -1)
-				{
-					sana = nimi.substring(0,i);
-					sana = sana.substring(0,1).toUpperCase()+sana.substring(1);
-					
-					apu = apu + sana + " " ;
-					
-					nimi = nimi.substring(i+1);
-					i = nimi.indexOf(' ');
-				}
-				nimi = nimi.substring(0,1).toUpperCase()+nimi.substring(1);
-				apu = apu + nimi;
-				// - j�lkeinen sana isolla alukirjaimella
-				nimi = apu;
-				apu="";
-				i = nimi.indexOf('-');
-				while (i != -1)
-				{
-					apu = apu + nimi.substring(0,i)+"-";
-					nimi = nimi.substring(i+1);
-					nimi = nimi.substring(0,1).toUpperCase()+nimi.substring(1);
-					
-					i = nimi.indexOf('-');
-				}
-				nimi = nimi.substring(0,1).toUpperCase()+nimi.substring(1);
-				apu = apu + nimi;
-				
-				paluu = apu;
-			}
-		}
-		
-		return paluu;
+		this.isbn = isbn;
 	}
 	public String getNimi() {
 		return nimi;
 	}
 	public void setNimi(String nimi) {
-		this.nimi =muokkaaNimi(nimi);
+		this.nimi = nimi;
 	}
 	public String getKirjoittaja() {
 		return kirjoittaja;
 	}
 	public void setKirjoittaja(String kirjoittaja) {
-		this.kirjoittaja = muokkaaHenkiloNimi(kirjoittaja);
+		this.kirjoittaja = kirjoittaja;
 	}
 	public int getPainos() {
 		return painos;
 	}
 	public void setPainos(int painos) {
-		this.painos = 0;
-		if (painos > 0)
-			this.painos = painos;
+		this.painos = painos;
 	}
 	public String getKustantaja() {
 		return kustantaja;
 	}
 	public void setKustantaja(String kustantaja) {
-		this.kustantaja =muokkaaNimi(kustantaja);
+		this.kustantaja = kustantaja;
+	}
+	@Override
+	public String toString() {
+		return "Kirja [isbn=" + isbn + ", nimi=" + nimi + ", kirjoittaja=" + kirjoittaja + ", painos=" + painos
+				+ ", kustantaja=" + kustantaja + "]";
 	}
 	
-	public String toString()
-	{
-		return isbn + "  " + nimi + " " + kirjoittaja +" " + painos + " " + kustantaja;
-	}
+	
 }
