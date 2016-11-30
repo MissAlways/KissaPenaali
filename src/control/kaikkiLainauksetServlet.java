e6249c586737d0e72087242edeff473120ff9c7d
MissAlways <ainakiurus@gmail.com>
2016-11-30 23:04:17 +0200

    Add kaikkiLainauksetServlet

---
+++ src/control/kaikkiLainauksetServlet.java
@@ -0,0 +1,54 @@
+package control;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Lainaus;
+import dao.LainausDao;
+
+/**
+ * Servlet implementation class kaikkiLainaukset
+ */
+@WebServlet("/kaikkiLainaukset")
+public class kaikkiLainauksetServlet extends HttpServlet {
+	private RequestDispatcher jsp;
+	private static final long serialVersionUID = 1L;
+    
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		ServletContext context = config.getServletContext();
+		jsp = context.getRequestDispatcher("/view/kaikkiLainaukset.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LainausDao lainausDao = new LainausDao();
+		List<Lainaus> lainausLista = lainausDao.haeKaikki();
+		request.setAttribute("lainauslista", lainausLista);
+		
+		jsp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
